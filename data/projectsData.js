01be2b3b255e706c07942c25945e5918c0e609fb
Varun Soni <varun1001soni@gmail.com>
2023-12-26 16:53:11 +0530

    added noteIt project

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,9 +1,29 @@
 export const projectsData = {
   projects: [
     {
+      title: "noteIt",
+      description:
+      "Your simple and stylish note companion! Elevate your note-taking experience with vibrant colored note cards along with easy editing, and hassle-free deletion of notes. Securely authenticate with Firebase and embrace a sleek dark theme option. Add titles, content, and choose from five vibrant colors to enhance your notes effortlessly!",
+      githubLink: "https://github.com/VarunSoni1001/noteit",
+      liveLink: "https://noteitbyvarun.vercel.app",
+      imageUrl: "/noteit_project.png",
+      skills: [
+        "Next.js",
+        "TypeScript",
+        "Responsive Design",
+        "Tailwind CSS",
+        "HTML",
+        "Firebase Authentication",
+        "Firebase Real-time database",
+        "CRUD operations",
+        "Dark Mode",
+        "NextUI",
+      ],
+    },
+    {
       title: "News. (NewsDot)",
       description:
-        "NewsDot is a cool news website made with Next.js and Tailwind CSS. It gets the latest news from the News API to keep you in the loop. With features like Light/Dark Mode and a clean, beautiful design that works well on any device because of its responsiveness, NewsDot makes staying informed easy and fun.",
+        "A cool news website made with Next.js and Tailwind CSS. It gets the latest news from the News API to keep you in the loop. With features like dark mode and a clean, beautiful design that works well on any device because of its responsiveness, NewsDot makes staying informed easy and fun.",
       githubLink: "https://github.com/VarunSoni1001/newsdot",
       liveLink: "https://newsdot.vercel.app",
       imageUrl: "/newsdot_project.png",
@@ -20,7 +40,7 @@ export const projectsData = {
       ],
     },
     {
-      title: "TODO List",
+      title: "TODO LIST",
       description:
         "A dynamic and responsive web application made with Next.js, created for managing to-do lists. The app supports task/to-do creation, marking them as completed, editing, and deletion. All changes are saved in Google's Firebase Realtime Database and authentication, and the app is styled with Tailwind CSS.",
       githubLink: "https://github.com/VarunSoni1001/todo-list-nextjs",
